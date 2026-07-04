9bc51126b8661e605fd6d18990cd82f94f75f5bb
Phant0m21 <danielvood2008@gmail.com>
2026-07-04 19:29:00 +0300

    Updated emojies

--- phantycoon/data.py
+++ phantycoon/data.py
@@ -1,7 +1,7 @@
 # Данные о кирках
 PICKAXES = {
     "Каменная кирка": {
-        "emoji": "<:stonepickaxe:1521830845340455072>",
+        "emoji": "<:stonepickaxe:1522995642996228106>",
         "cooldown": 4.2,
         "amount_min": 1,
         "amount_max": 1,
@@ -10,7 +10,7 @@ PICKAXES = {
         "price": 0
     },
     "Железная кирка": {
-        "emoji": "<:ironpickaxe:1521827763022073916>",
+        "emoji": "<:ironpickaxe:1522995651095298129>",
         "cooldown": 3.8,
         "amount_min": 1,
         "amount_max": 2,
@@ -19,7 +19,7 @@ PICKAXES = {
         "price": 15000
     },
     "Золотая кирка": {
-        "emoji": "<:goldenpickaxe:1521827761814110269>",
+        "emoji": "<:goldenpickaxe:1522995649828618292>",
         "cooldown": 3.5,
         "amount_min": 1,
         "amount_max": 3,
@@ -28,7 +28,7 @@ PICKAXES = {
         "price": 75000
     },
     "Алмазная кирка": {
-        "emoji": "<:diamondpickaxe:1521827760647966800>",
+        "emoji": "<:diamondpickaxe:1522995648511868928>",
         "cooldown": 3.1,
         "amount_min": 2,
         "amount_max": 4,
@@ -37,7 +37,7 @@ PICKAXES = {
         "price": 350000
     },
     "Незеритовая кирка": {
-        "emoji": "<:netheritepickaxe:1521827759465431172>",
+        "emoji": "<:netheritepickaxe:1522995647073226843>",
         "cooldown": 2.6,
         "amount_min": 3,
         "amount_max": 5,
@@ -49,31 +49,31 @@ PICKAXES = {
 
 ORES = {
     "Уголь": {
-        "emoji": "<:coalore:1521827756680286208>",
+        "emoji": "<:coalore:1522995644506046524>",
         "chance": 60,
         "price_min": 5,
         "price_max": 7
     },
     "Медь": {
-        "emoji": "<:copperore:1521827769137234010>",
+        "emoji": "<:copperore:1522995656967586013>",
         "chance": 24,
         "price_min": 15,
         "price_max": 17
     },
     "Железо": {
-        "emoji": "<:ironore:1521827767308783638>",
+        "emoji": "<:ironore:1522995655440863482>",
         "chance": 12,
         "price_min": 32,
         "price_max": 37
     },
     "Золото": {
-        "emoji": "<:goldore:1521827765983252612>",
+        "emoji": "<:goldore:1522995653750558810>",
         "chance": 3.7,
         "price_min": 72,
         "price_max": 83
     },
     "Алмаз": {
-        "emoji": "<:diamond:1521827757959413821>",
+        "emoji": "<:diamond:1522995645877846027>",
         "chance": 0.3,
         "price_min": 250,
         "price_max": 310
